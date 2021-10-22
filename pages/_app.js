92199c6dec4d5c6a5df96c3822539d6c3c211d72
Santiago Nuñez <tau150@hotmail.com>
2021-10-22 16:25:26 -0300

    theme and i18n config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
+import * as React from 'react'
 import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { IntlProvider } from 'react-intl'
+import { useRouter } from 'next/dist/client/router'
+import intlESmessages from '../content/locale/es.json'
+import intlENmessages from '../content/locale/en.json'
+
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
+  const { locale } = useRouter();
+  const [shortLocale] = locale ? locale.split("-") : ["en"];
+
+  const langs = {
+    es: intlESmessages,
+    en: intlENmessages
+  }
+  const messages = langs[shortLocale]
+
+  return (
+    <IntlProvider
+      locale={shortLocale}
+      messages={messages}
+      onError={() => null}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps}/>
+      </ChakraProvider>
+    </IntlProvider>
+  )
+}
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#FF0049",
+      secondary: "#002B45",
+      terciary: "#FFF1E5"
+    },
+  },
+})
+
+export default theme;
